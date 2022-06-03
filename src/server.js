2e0e0f958767dcdff78407309d99f97a610229e4
Lucidreline <casta.ma502@gmail.com>
2022-06-03 15:14:31 -0700

    feat(all): began Docker Dev Environment

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 
 const app = express()
 
+app.get('/', (req, res) => res.send('<h2>Welcome to Lamp World!</h2>'))
+
 app.listen(process.env.PORT, () =>
   // eslint-disable-next-line no-console
   console.log(`💡 Lamp World Running on port ${process.env.PORT}`),
