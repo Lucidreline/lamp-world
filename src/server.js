b68ef23daf8c8c26b04cac65b060cc8176ecaec8
Lucidreline <casta.ma502@gmail.com>
2022-06-04 01:01:43 -0700

    feat(docker-compose): added different docker-compose files for prod and dev

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const app = express()
 
-app.get('/', (req, res) => res.send('<h2>Welcome to Lamp World!</h2>'))
+app.get('/', (req, res) => res.send('<h2>Welcome to Lamp World!!</h2>'))
 
 app.listen(process.env.PORT, () =>
   // eslint-disable-next-line no-console
